ed5bd998ceccf5e9198477f7b7d666072cd6d897
Aleksander Nitecki <ixendr@itogi.re>
2015-09-11 13:51:10 +0200

    HTML-izer: use Jukugo (I think?) ruby.

--- htmlizer.py
+++ htmlizer.py
@@ -1,31 +1,72 @@
 #!/usr/bin/env python3
 
+import re
 import sys
 
 
-header = '''\
+html_header = '''\
 <!doctype html>
 <meta charset="utf-8">
 
 <style>
 body {
 	font-family: "Kozuka Mincho Pr6N R";
+	page-break-inside: avoid;
+}
+
+.page-break {
+	page-break-after: always;
 }
 </style>
 
+<p>\
 '''
 
+
+furigana_re = re.compile(r'\[(.*?)\|(.*?)\]')  # Match "[kanji|kana]" format.
+
+def furigana_re_replacer(match):
+	kanji, kana = map(
+		lambda s: s.split('.'),
+		match.groups()
+	)
+
+	return ''.join((
+		'<ruby>',
+		'<rb>'.join(kanji),
+		'<rt>',
+		'<rt>'.join(kana),
+		'</ruby>'
+	))
+
+def furiganize(s):
+	return furigana_re.sub(furigana_re_replacer, s)
+
+
+def stripped_and_empty_lines_collapsed(it):
+	last_line_empty = False
+
+	for line in it:
+		line = line.rstrip('\n')
+
+		if line == '' and last_line_empty:
+			continue
+
+		last_line_empty = (line == '')
+
+		yield line
+
+
 def main():
-	print(header)
+	print(html_header)
 
-	for line in sys.stdin:
-		html_line = (line
-			.replace('[', '<ruby><rb>')
-			.replace('|', '</rb><rt>')
-			.replace(']', '</rt></ruby>')
-		)
-
-		print(html_line, end='<br>')
+	for line in stripped_and_empty_lines_collapsed(sys.stdin):
+		if line == '':
+			print('<p>')
+		elif line == '~':
+			print('<div class="page-break">~</div>')
+		else:
+			print(furiganize(line), end='<br>\n')
 
 
 if __name__ == '__main__':
