dbcf758a4711ea92307c2a618e0a02d8783479bd
Aleksander Nitecki <ixendr@itogi.re>
2015-09-11 19:33:44 +0200

    Refactor: extract.

--- htmlizer.py
+++ htmlizer.py
@@ -2,26 +2,12 @@
 
 import sys
 
-from markup_processor import furiganize
-from helpers import stripped_and_empty_lines_collapsed
+from markup_processor import process
 from formatters.html import HtmlFormat
 
 
-out_format = HtmlFormat
-
-
 def main():
-	print(out_format.header)
-
-	for line in stripped_and_empty_lines_collapsed(sys.stdin):
-		if line == '':
-			print(out_format.new_paragraph)
-		elif line == '~':
-			print(out_format.page_separator)
-		else:
-			print(furiganize(line, out_format.ruby_formatter), end=out_format.new_line)
-
-	print(out_format.footer)
+	process(HtmlFormat, sys.stdin, sys.stdout)
 
 
 if __name__ == '__main__':
--- markup_processor.py
+++ markup_processor.py
@@ -24,3 +24,22 @@ def furiganize(text, formatter):
 	replacer = lambda match: furigana_re_replacer(match, formatter)
 
 	return furigana_re.sub(replacer, text)
+
+
+# ---
+
+from helpers import stripped_and_empty_lines_collapsed
+
+
+def process(out_format, in_file, out_file):
+	print(out_format.header, file=out_file)
+
+	for line in stripped_and_empty_lines_collapsed(in_file):
+		if line == '':
+			print(out_format.new_paragraph, file=out_file)
+		elif line == '~':
+			print(out_format.page_separator, file=out_file)
+		else:
+			print(furiganize(line, out_format.ruby_formatter), end=out_format.new_line, file=out_file)
+
+	print(out_format.footer, file=out_file)
